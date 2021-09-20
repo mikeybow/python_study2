faa900bf4848126a13887681dbe8056aaf13cf3d
Michael Bowman <micbowman25@gmail.com>
2021-09-20 17:02:52 -0500

    finished practice

---
+++ app.py
@@ -0,0 +1,8 @@
+has_high_income = True
+has_high_credit = False
+
+if has_high_income and has_high_credit:
+    print('Eligible For Loan')
+else:
+    print('Not Eligible For Loan')
+print('Thank you and have a nice day!')
